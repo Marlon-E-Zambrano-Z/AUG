575350551030a20abda9b9176326345d9426a95e
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-22 22:31:14 -0500

    Modificación
    Se establecieron las rutas con una librería llamada react router esto simula que la SPA funciona con rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screen/Login";
+import Register from "./screen/Register";
 
 function App() {
+  
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route 
+        path="/login" 
+        element={<Login/>}
+        />
+    
+        <Route
+        path="/register"
+        element={<Register/>}
+        />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 

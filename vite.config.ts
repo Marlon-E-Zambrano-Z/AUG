54e9d8dc3455b82dfcb5d5b1a79dff67e451725f
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-30 14:19:33 -0500

    Modificación
    Se agregó configuración para less y css modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css:{
+    modules:{
+      generateScopedName: "[name]__[local]__[hash:base64:5]"
+    },
+    preprocessorOptions:{
+      less: {
+        javascriptEnabled : true
+      }
+    }
+  },
   server:{
     proxy:{
       '/api':{

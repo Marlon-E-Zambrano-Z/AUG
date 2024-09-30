884a94915d41e7d70a4e8a4916aaac487e2ec1f3
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-30 14:08:21 -0500

    Creación
    Funciones que se encargan de las conexiones con el backend en la vista determinada

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,5 +1,40 @@
 import axios from "axios"
+import bcrypt from "bcryptjs"
+/*
+Entradas: 
+  - idEMail: Puede ingresar un id o un correo electrónico
+  - password: Puede ingresar una contraseña que debe cumplir minimamente 
+    con
+    8 carácteres, 
+    una letra mayúscula, 
+    una minuscula, 
+    un carácter especial entre estos @$!%*?&
+    un número
 
-export const = async (idEmail, password) => {
-  axios.post("/api");
+Salidas
+  - Un objeto con dos propiedades
+    * error: por ahora no se ha definido sí será un objeto o un mensaje como string
+      así siendo un objeto almacenará la información por defecto que proporciona javascript a un objeto,
+      de lo contrario sí es un mensaje este sería un string
+    * status: un booleano que define sí la respuesta tuvo problemas o no, su valor depende de ello
+*/
+export const sendCredentials : object = async (idEmail : string, password: string) => {
+  try{
+  const SALT = bcrypt.genSaltSync(10) 
+  const HASHED_PASSWD : string = bcrypt.hashSync(password, SALT)
+  
+  await axios.post("/api",{
+    idEmail,
+    password : HASHED_PASSWD
+  })
+  }catch (error){
+    return {
+      error : error, 
+      status: false
+    }
+  }
+  return {
+    error: null,
+    status: true
+  }
 }

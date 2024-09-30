6f6fbe683c3b41acd46ff2e74f74b7dfc117ad57
Marlon E. Zambrano Z. <marlon.zambrano@correounivalle.edu.co>
2024-09-30 14:25:06 -0500

    Modificación
    Se agregará comentarios para especificar mejor lo que hace cada expresión regular

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,1 +1,4 @@
-export const idEmail : RegExp = /\d+|^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ 
+
+export const idEmail : RegExp = /\d+|^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+export const password : RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/ 
